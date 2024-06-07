5e99953fff288886e811a468636f34f3d5833318
StanFrad <93010595+StanFrad@users.noreply.github.com>
2024-06-07 10:21:56 +0400

    Update config-appmenu.h.cmake

--- config-appmenu.h.cmake
+++ config-appmenu.h.cmake
@@ -4,7 +4,7 @@
 /* Define if you have LibTaskManager at all */
 // #cmakedefine01 HAVE_LibTaskManager)
 
-#cmakedefine KF5_CURRENTMINOR_VERSION @KF5_CURRENTMINOR_VERSION@
+#cmakedefine KF6_CURRENTMINOR_VERSION @KF6_CURRENTMINOR_VERSION@
 
 #cmakedefine LibTaskManager_CURRENTMINOR_VERSION @LibTaskManager_CURRENTMINOR_VERSION@
 
